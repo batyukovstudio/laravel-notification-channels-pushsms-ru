f4d4d9986cba43d467bb86a322dcc8d0092a5dd1
Eugene Batyukov <eugene@batyukov.ru>
2024-07-05 13:58:28 +0500

    feat: Added content prefix

--- Config/pushsms.php
+++ Config/pushsms.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'access_token' => env('PUSHSMS_TOKEN', null),
-    'sender_name' => env('PUSHSMS_SENDER_NAME', null),
-    'timeout' => 5,
+    'access_token'    => env('PUSHSMS_TOKEN', null),
+    'sender_name'     => env('PUSHSMS_SENDER_NAME', null),
+    'timeout'         => 5,
     'connect_timeout' => 5,
-
+    'content_prefix'  => env('PUSHSMS_CONTENT_PREFIX', null),
 ];
--- src/Notifications/PushSmsNotification.php
+++ src/Notifications/PushSmsNotification.php
@@ -6,12 +6,13 @@ use Illuminate\Notifications\Notification;
 use NotificationChannels\PushSMS\ApiActions\PushSmsMessage;
 use NotificationChannels\PushSMS\Notifications\Interfaces\PushSmsable;
 
-abstract class PushSmsNotification extends Notification implements PushSmsable
-{
+abstract class PushSmsNotification extends Notification implements PushSmsable {
+
     protected string $content;
 
-    public function toPushSms($notifiable): PushSmsMessage
-    {
-        return PushSmsMessage::create()->content($this->content);
+    public function toPushSms($notifiable): PushSmsMessage {
+
+        $content = config('pushsms.content_prefix') . $this->content;
+        return PushSmsMessage::create()->content($content);
     }
 }
